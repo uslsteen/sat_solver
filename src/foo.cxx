5f1460053804a469b6e7544ce2d12385fb22b894
uslstenn <shurygin0171@gmail.com>
2024-01-16 19:00:29 +0300

    Initial build setup

---
+++ src/foo.cxx
@@ -0,0 +1,13 @@
+// Global module fragment where #includes can happen
+module;
+#include <iostream>
+
+// first thing after the Global module fragment must be a module command
+export module foo;
+
+export class foo {
+public:
+    foo() = default;
+    ~foo() = default;
+    void helloworld() { std::cout << "hello world\n"; }
+};
